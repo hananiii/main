b9863026549eb3892ebbef5e4fc856216a181a3e
Ruoli <angrl@zen.com.my>
2019-06-12 16:30:41 +0800

    Test documentation comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,11 @@ export class AppComponent {
   constructor() {
   }
 
+  /**
+   *
+   *
+   * @memberof AppComponent
+   */
   ngOnInit() {
   }
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,21 +10,62 @@ import { AuthService } from 'src/services/shared-service/auth.service';
 })
 export class LoginComponent implements OnInit {
 
+  /**
+   *
+   *
+   * @type {boolean}
+   * @memberof LoginComponent
+   */
   public showPassword: boolean = false;
+  /**
+   *
+   *
+   * @type {boolean}
+   * @memberof LoginComponent
+   */
   public valueOfCheck: boolean;
+  /**
+   *
+   *
+   * @type {string}
+   * @memberof LoginComponent
+   */
   public emailValue: string;
+  /**
+   *
+   *
+   * @type {string}
+   * @memberof LoginComponent
+   */
   public passValue: string;
 
+  /**
+   *
+   *
+   * @memberof LoginComponent
+   */
   public formGroupValidation = new FormGroup({
     email: new FormControl(null, [Validators.required, Validators.email]),
     pass: new FormControl(null, [Validators.required]),
   });
 
+  /**
+   *
+   *
+   * @returns
+   * @memberof LoginComponent
+   */
   getErrorMessage() {
     return this.formGroupValidation.controls['email'].hasError('required') ? 'Please enter your email address' :
       this.formGroupValidation.controls['email'].hasError('email') ? 'Not a valid email' : '';
   }
 
+  /**
+   *
+   *
+   * @returns
+   * @memberof LoginComponent
+   */
   getPassErrorMessage() {
     return this.formGroupValidation.controls['pass'].hasError('required') ? 'Please enter your password' : '';
   }
@@ -32,6 +73,11 @@ export class LoginComponent implements OnInit {
   constructor(private _auth: AuthService,
     private router: Router) { }
 
+  /**
+   *
+   *
+   * @memberof LoginComponent
+   */
   ngOnInit() {
     if ((localStorage.getItem('user_email') !== null) && (localStorage.getItem('user_pass') !== null)) {
       this.valueOfCheck = true;
@@ -43,13 +89,30 @@ export class LoginComponent implements OnInit {
     this._auth.logout();
   }
 
+  /**
+   *
+   *
+   * @memberof LoginComponent
+   */
   showPasswordKey() {
     this.showPassword = true;
   }
+  /**
+   *
+   *
+   * @memberof LoginComponent
+   */
   hidePasswordKey() {
     this.showPassword = false;
   }
 
+  /**
+   *
+   *
+   * @param {string} email
+   * @param {string} pass
+   * @memberof LoginComponent
+   */
   signIn(email: string, pass: string) {
     this._auth.login(email, pass)
       .subscribe(data => {
@@ -59,6 +122,14 @@ export class LoginComponent implements OnInit {
       );
   }
 
+  /**
+   *
+   *
+   * @param {*} event
+   * @param {string} email
+   * @param {string} pass
+   * @memberof LoginComponent
+   */
   rememberMe(event: any, email: string, pass: string) {
     if (email !== "" || pass !== "") {
       localStorage.setItem('user_email', email);
