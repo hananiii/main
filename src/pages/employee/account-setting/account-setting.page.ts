df47a2fee6d5665f91680291577e4deb19644dda
Ruoli <angrl@zen.com.my>
2019-09-18 17:07:11 +0800

    Changed design of account-setting page

--- src/pages/employee/account-setting/account-setting.page.ts
+++ src/pages/employee/account-setting/account-setting.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { APIService } from 'src/services/shared-service/api.service';
-import { Subscription } from 'rxjs';
 
 /**
  * Account Setting Page
@@ -16,43 +14,16 @@ import { Subscription } from 'rxjs';
 export class AccountSettingPage implements OnInit {
 
     /**
-     * This local private property is used to set subscription
-     * @private
-     * @type {Subscription}
-     * @memberof AccountSettingPage
-     */
-    private subscription: Subscription = new Subscription();
-
-    /**
      *Creates an instance of AccountSettingPage.
-     * @param {APIService} apiService
      * @memberof AccountSettingPage
      */
-    constructor(private apiService: APIService) { }
+    constructor() { }
 
     /**
      * Initial method
      * @memberof AccountSettingPage
      */
     ngOnInit() {
-        this.subscription = this.apiService.get_user_profile_list().subscribe(
-            (data: any[]) => {
-                // this.employeeList = data;
-            },
-            error => {
-                if (error) {
-                    window.location.href = '/login';
-                }
-            }
-        );
 
-    }
-
-    /**
-     * This method is used to destroy subscription of user profile list
-     * @memberof AccountSettingPage
-     */
-    ngOnDestroy() {
-        this.subscription.unsubscribe();
     }
 }
