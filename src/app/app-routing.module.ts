34f84fe8fcb88012b81a199bd26976282bd1020f
Hanani Rafei <hanani@zen.com.my>
2020-07-01 14:33:17 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,22 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  // {
+  //   path: 'login1',
+  //   loadChildren: () => import('$admin-root/src/app/login/login.module').then( m => m.LoginModule )
+  // },
+  // {
+  //   path: 'test1',
+  //   loadChildren: () => import('@hanani/eleave-user/src/app/app.module').then(m => m.AppModule)
+  // },
+  // {
+  //   path: 'admin',
+  //   loadChildren: () => import('@zencomputersystems/eleave-admin/src/app/app.module').then( m => m.AppModule)
+  // },
+  // {
+  //   path: 'user',
+  //   loadChildren: () => import('@zencomputersystems/eleave-admin/src/app/app.module').then(m => m.AppModule)
+  // },
   {
     path: '',
     redirectTo: 'home',
